9c231d6d8f64a2292653fae07102cb395040fe06
Nazarii Boichyshyn <boichyshyn@gmail.com>
2021-04-26 01:16:18 +0300

    Dependency Injection Method
    Dependency Injection Method

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+namespace DependencyInjectionMethod
+{
+    class Program
+    {
+
+        public interface ILogger
+        {
+            void LogInfo(String info);
+        }
+
+        public class TextLogger : ILogger
+        {
+            public void LogInfo(string info)
+            {
+                // Log data into a Text file.
+                WriteLine(info);
+            }
+        }
+        public class XMLLogger : ILogger
+        {
+            public void LogInfo(string info)
+            {
+                // Log data into a XML file.
+                WriteLine(info);
+            }
+        }
+
+
+
+        public class User
+        {
+            public void SaveData(ILogger loggerType)
+            {
+                loggerType.LogInfo($"Save data {loggerType}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            User _user = new User();
+
+            _user.SaveData(new TextLogger());
+
+            _user.SaveData(new XMLLogger());
+
+        }
+    }
+}
